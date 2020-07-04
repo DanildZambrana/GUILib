248c918a2e4c063a8b722f063ac481bebefdc894
DanildZambrana <42362162+DanildZambrana@users.noreply.github.com>
2020-07-03 21:25:37 -0600

    Execute code from BaseHolder#executeClose()

--- src/main/java/io/github/danildzambrana/guilib/handlers/SinglePageHandler.java
+++ src/main/java/io/github/danildzambrana/guilib/handlers/SinglePageHandler.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,5 +31,19 @@ public class SinglePageHandler extends MenuHandler implements Listener {
 
         SingleHolder singleHolder = (SingleHolder) event.getView().getTopInventory().getHolder();
         super.execute(singleHolder, event, player);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (!(event.getView().getTopInventory().getHolder() instanceof SingleHolder)){
+            return;
+        }
+
+        if(!(event.getPlayer() instanceof Player)){
+            return;
+        }
+
+        SingleHolder singleHolder = (SingleHolder) event.getView().getTopInventory().getHolder();
+        singleHolder.executeClose(event);
     }
 }
