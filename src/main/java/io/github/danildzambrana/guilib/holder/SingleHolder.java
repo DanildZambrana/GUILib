1f72c763063b70320b7627f28efd4b20dfe1cfac
DanildZambrana <42362162+DanildZambrana@users.noreply.github.com>
2020-07-03 21:27:14 -0600

    Fixing the bug that the buttons are not put in the menu.

--- src/main/java/io/github/danildzambrana/guilib/holder/SingleHolder.java
+++ src/main/java/io/github/danildzambrana/guilib/holder/SingleHolder.java
@@ -1,12 +1,14 @@
 package io.github.danildzambrana.guilib.holder;
 
+import io.github.danildzambrana.guilib.button.Button;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class SingleHolder extends BaseHolder implements InventoryHolder {
+public class SingleHolder extends BaseHolder {
     public SingleHolder(int size, String title, boolean blockAllSlots) {
         super(size, title, new HashMap<>(), blockAllSlots);
     }
@@ -15,7 +17,9 @@ public class SingleHolder extends BaseHolder implements InventoryHolder {
     public Inventory getInventory() {
         Inventory inventory = Bukkit.createInventory(this, super.getSize(), super.getTitle());
 
-        super.getButtonMap().forEach(inventory::setItem);
+        for (Map.Entry<Integer, Button> entry : super.getButtonMap().entrySet()) {
+            inventory.setItem(entry.getKey(), entry.getValue().getItemStack());
+        }
 
         return inventory;
     }
